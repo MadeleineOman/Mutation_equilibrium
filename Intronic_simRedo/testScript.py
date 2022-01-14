0e015f1351cfa0f20d6585edb21ded98ea122dcb
Madeleine Oman <madeleine.oman@mail.utoronto.ca>
2022-01-14 17:53:46 -0500

    pruning old files and adding/updating new ones in the intronic sim redo folder. also added a readme

--- Intronic_simRedo/testScript.py
+++
@@ -1,5 +0,0 @@
-import sys 
-from datetime import datetime
-timestamp = datetime.now().strftime("%Y/%m/%d_%H:%M:%S").replace("/", "_").replace(":", "_")
-print(sys.argv[1], sys.argv[2], sys.argv[3],sys.argv[4])
-print(timestamp)
